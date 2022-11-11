60b2f567ad286be3584e9b272a7e44a354f95b20
110b26227hana <114041580+110b26227hana@users.noreply.github.com>
2022-11-11 13:12:00 +0800

    Create  Class03.java

---
+++ Class03.java
@@ -0,0 +1,28 @@
+class CCircle
+{
+  protected static double pi = 3.14;
+  protected double radius;
+  public void show()
+  {
+    System.out.println("area="+pi*radius*ardius);
+  }
+}
+class CCoin extens CCircle
+{
+  private int value;
+  public CCoin(double r, int v)
+  {
+    radius = r;
+    value = v;
+    System.out.println("radius="+radiusv+, value=+value);
+  }
+}
+public class app10_5
+{
+  public static void main(String arg[])
+  {
+    CCoin coin = new CCoin(2.5, 10);
+    coin.show();
+  }
+}
+   
